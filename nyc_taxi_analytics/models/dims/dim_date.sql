df4d445fd1850a225e7b080bf5b90d8f290f9322
sohaib <sohaibt884@gmail.com>
2025-06-29 22:25:44 +0500

    fix: dim_date query

--- nyc_taxi_analytics/models/dims/dim_date.sql
+++ nyc_taxi_analytics/models/dims/dim_date.sql
@@ -1,20 +1,23 @@
 {{ config(materialized='table') }}
 
-select
-  distinct 
-  date(try_to_timestamp(tpep_pickup_datetime)) as date,
-  dayofweek(try_to_timestamp(tpep_pickup_datetime)) as weekday_num,
-  case 
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 1 then 'Sunday'
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 2 then 'Monday'
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 3 then 'Tuesday'
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 4 then 'Wednesday'
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 5 then 'Thursday'
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 6 then 'Friday'
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) = 7 then 'Saturday'
-  end as weekday_name,
-  case 
-    when dayofweek(try_to_timestamp(tpep_pickup_datetime)) in (1, 7) then true
-    else false
-  end as is_weekend
-from {{ ref('stg_yellow_tripdata') }}
+SELECT
+  DISTINCT
+  DATE(TRY_TO_TIMESTAMP(tpep_pickup_datetime)) AS date,
+  EXTRACT(dayofweek FROM TRY_TO_TIMESTAMP(tpep_pickup_datetime)) AS weekday_num,
+  DECODE(
+    EXTRACT(dayofweek FROM TRY_TO_TIMESTAMP(tpep_pickup_datetime)),
+    1, 'Sunday',
+    2, 'Monday',
+    3, 'Tuesday',
+    4, 'Wednesday',
+    5, 'Thursday',
+    6, 'Friday',
+    7, 'Saturday'
+  ) AS weekday_name,
+  DECODE(
+    EXTRACT(dayofweek FROM TRY_TO_TIMESTAMP(tpep_pickup_datetime)),
+    1, TRUE,
+    7, TRUE,
+    FALSE
+  ) AS is_weekend
+FROM {{ ref('stg_yellow_tripdata') }}
